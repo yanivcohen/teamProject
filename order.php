6070c47151c6047fa50a99c8d37924789f1f3ed5
Yaniv <yaniv@Yanivs-MacBook-Pro.local>
2017-04-21 13:18:08 -0700

    changed pay to php7

--- order.php
+++ order.php
@@ -158,12 +158,12 @@ else if ($_SERVER['REQUEST_METHOD'] === 'PUT'){
 		//	$updateResult = $empcollection->updateOne(
 		//	[ '_id' => $requestArray[3]],
 		//	['$set' => ['status'=>"paid"]]);
-			$IDQuery = array('_id' => $requestArray[3]);
-			$doc=$empcollection->findOne($IDQuery);
+			//$IDQuery = array('_id' => $requestArray[3]);
+			//$doc=$empcollection->findOne($IDQuery);
 			//foreach ($cursor as $doc) {
 			//var_dump($doc);
 			//}
-		    if (count($doc)>0){
+		    /*if (count($doc)>0){
 				$myObjGet = new stdClass();
 		
 				$myObjGet->id =$doc['_id'] ;
@@ -179,7 +179,38 @@ else if ($_SERVER['REQUEST_METHOD'] === 'PUT'){
 
 				echo $myJSON;
 		
+			}*/
+			$filter=['_id'=>$requestArray[2]];
+
+                       // $options = [ 'projection' => ['_id' => 0],];
+		//	var_dump($filter);
+			$query = new MongoDB\Driver\Query($filter);
+			$cursor = $mng->executeQuery('starbucks.orders', $query);
+			foreach ($cursor as $doc) {
+    			  //	var_dump($doc);
 			}
+
+                //	echo "the id from the retruend find is" . $doc->_id   ;
+			//$doc=$empcollection->findOne($IDQuery);
+			//foreach ($cursor as $doc) {
+			//var_dump($doc);
+			//}
+		    if (count($doc)>0){
+				$myObjGet = new stdClass();
+		
+				$myObjGet->id =$doc->_id ;
+		
+				$myObjGet->items = [['milk'=>$doc->milk,'name'=>$doc->name,'qty'=>$doc->qty , 'size'=>$doc->size]];
+				$myObjGet->links=['payment'=>"http://localhost/phpmongodb/order/".$doc->_id."/pay",'Order'=>"http://localhost/phpmongodb/order/".$doc->_id];
+				$myObjGet->location=$doc->location;
+				$myObjGet->status=$doc->status;
+				$myObjGet->message=$doc->message;
+				//var_dump($OneOrderArray);
+				$myJSON = json_encode($myObjGet);
+		
+
+				echo $myJSON;
+			
 			}
 
 ?>
