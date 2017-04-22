0185c9d9bb95fb09c6a3335d86aec9804faf4c17
Yaniv <yaniv@Yanivs-MacBook-Pro.local>
2017-04-22 16:55:30 -0700

    add orders

--- order.php
+++ order.php
@@ -28,7 +28,7 @@ $myObj = new stdClass();
 			echo $myJSON;
 
 }
-else if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+else if ($_SERVER['REQUEST_METHOD'] === 'GET' and $requestArray[2] !="orders"){
                     //    echo "i am in get";
 			//echo "GET Request the current state of the order specified by the URI.";
 			//echo $_SERVER['REQUEST_URI'];
@@ -212,6 +212,49 @@ else if ($_SERVER['REQUEST_METHOD'] === 'PUT'){
 				echo $myJSON;
 			
 			}
-		}
+		}else if ($_SERVER['REQUEST_METHOD'] === 'GET'){
+				echo "i am in orders";
+		          //    echo "i am in get";
+			//echo "GET Request the current state of the order specified by the URI.";
+			//echo $_SERVER['REQUEST_URI'];
+			$requestArray=explode ('/', $_SERVER['REQUEST_URI'] );
+		  //      echo var_dump($_SERVER['REQUEST_URI']);
+		//	echo "the request id is".  $requestArray[2];
+			//$IDQuery = array('_id' => $requestArray[2]);
+
+			$filter=['_id'=>$requestArray[2]];
+
+                       // $options = [ 'projection' => ['_id' => 0],];
+		//	var_dump($filter);
+			$query = new MongoDB\Driver\Query($filter);
+			$cursor = $mng->executeQuery('starbucks.orders', $query);
+			foreach ($cursor as $doc) {
+    			  //	var_dump($doc);
+			}
+
+                //	echo "the id from the retruend find is" . $doc->_id   ;
+			//$doc=$empcollection->findOne($IDQuery);
+			//foreach ($cursor as $doc) {
+			//var_dump($doc);
+			//}
+		    if (count($doc)>0){
+				$myObjGet = new stdClass();
+		
+				$myObjGet->id =$doc->_id ;
+		
+				$myObjGet->items = [['milk'=>$doc->milk,'name'=>$doc->name,'qty'=>$doc->qty , 'size'=>$doc->size]];
+				$myObjGet->links=['payment'=>"http://localhost/phpmongodb/order/".$doc->_id."/pay",'Order'=>"http://localhost/phpmongodb/order/".$doc->_id];
+				$myObjGet->location=$doc->location;
+				$myObjGet->status=$doc->status;
+				$myObjGet->message=$doc->message;
+				//var_dump($OneOrderArray);
+				$myJSON = json_encode($myObjGet);
+		
+
+				echo $myJSON;
+		
+			}
+
+}
 
 ?>
